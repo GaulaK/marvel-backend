b5ea017739d7558abbc49391f7298c018a5f9e5e
Gauthier Portefaix <portefaixgauthier@gmail.com>
2023-02-19 19:09:12 +0100

    clear User.js

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-//packages
+// Packages
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const uid2 = require("uid2");
+
 // Models
 const User = require("../models/User");
+
 // Middlewares
 const isAuthenticated = require("../middlewares/isAuthenticated");
-const { default: axios } = require("axios");
 
 router.post("/user/signup", async (req, res) => {
   const { username, email, password } = req.body;
@@ -72,6 +73,7 @@ router.post("/user/login", async (req, res) => {
         error: { message: `Connection Failed : Wrong email or password` },
       });
     }
+    // Verify Password
     const hash = SHA256(password + UserTryToConnect.salt).toString(encBase64);
     if (hash === UserTryToConnect.hash) {
       return res.json({
@@ -91,7 +93,6 @@ router.post("/user/login", async (req, res) => {
 });
 
 router.post("/user/favorites/add", isAuthenticated, async (req, res) => {
-  // console.log(req.user);
   const UserToModify = req.user;
   if (Object.keys(req.body)[0]) {
     const tabToModify = Object.keys(req.body)[0];
@@ -102,8 +103,6 @@ router.post("/user/favorites/add", isAuthenticated, async (req, res) => {
       });
     }
     try {
-      //Verify if isnt already in favorite
-
       UserToModify.favorites[tabToModify].push(value);
       await UserToModify.save();
       return res.json({
@@ -118,8 +117,8 @@ router.post("/user/favorites/add", isAuthenticated, async (req, res) => {
   }
 });
 router.post("/user/favorites/remove", isAuthenticated, async (req, res) => {
-  // console.log("user:", req.user);
   const UserToModify = req.user;
+
   if (Object.keys(req.body)[0]) {
     const tabToModify = Object.keys(req.body)[0];
     const value = req.body[tabToModify];
@@ -144,7 +143,6 @@ router.post("/user/favorites/remove", isAuthenticated, async (req, res) => {
         favorites: UserToModify.favorites,
       });
     } catch (error) {
-      console.log(error);
       return res.status(400).json({ error: error });
     }
   } else {
@@ -153,11 +151,18 @@ router.post("/user/favorites/remove", isAuthenticated, async (req, res) => {
 });
 
 router.get("/user/favorites/get", isAuthenticated, async (req, res) => {
-  // console.log("user:", req.user);
-
   try {
     const UserGetFavorites = req.user;
-    console.log(UserGetFavorites);
+
+    /*
+     * Favorites are saved with all the content (because comics cannot be searched by their id*),
+     * so we can return the raw data of 'Favorite' object without missing any data
+     *
+     * * we don't only save the title of comics because there is lot of issues with special characters encoding
+     * Strength : Less API calls
+     * Weakness: the API loses some interest, it would be better to stock all datas in the DB linked to the server
+     *
+     */
     res.json(UserGetFavorites.favorites);
   } catch (error) {
     console.log(error.response.data);
